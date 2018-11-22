5cb6c71526763bd6a766c80ea324c0557d04095e
Wil Cook <wil.cook.is@gmail.com>
2018-11-22 19:19:09 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length > 2 
+    last = array.pop
+    sentence = array.join(", ")
+    sentence << ", and #{last}"
+    return sentence
+  elsif array.length == 2 
+    return array.join(" and ")
+  else 
+    return array.join
+  end
 end
